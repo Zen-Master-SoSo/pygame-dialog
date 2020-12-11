2d50b24ecb412bc20ea1dea276a543f48c169802
Zen Master SoSo <zen_master_soso@dridesign.sh.cn>
2020-12-11 16:43:46 +0800

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from pygame_dialog.pygame_dialog import \
+	GridLayout, HorizontalLayout, VerticalLayout, \
+	Label, Button, Textbox, Dialog, \
+	ALIGN_CENTER, ALIGN_LEFT, ALIGN_RIGHT, \
+	STATE_NORMAL, STATE_HOVER, STATE_FOCUS, \
+	NO_POSITION
+
+
